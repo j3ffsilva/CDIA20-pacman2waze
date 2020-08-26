3b283a731837fe3b627fb9b5f6893c71ff46fb18
Jefferson O. Silva <silva.o.jefferson@gmail.com>
2020-08-26 08:21:18 -0300

    Ajustes nos comentários e chamadas de função

--- semana03/pacman.py
+++ semana03/pacman.py
@@ -4,26 +4,27 @@
 from turtle import *
 
 def ler_matriz():
-    return [[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0],\
-            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0],\
-            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0],\
-            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0],\
-            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0],\
-            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0],\
-            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0],\
-            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0],\
-            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0],\
-            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0],\
-            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0],\
-            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0],\
-            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0],\
-            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0],\
-            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0],\
-            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0],\
-            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0],\
-            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0],\
-            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0],\
-            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0]]
+    return [[0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0],\
+            [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0],\
+            [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0],\
+            [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0],\
+            [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0],\
+            [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0],\
+            [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0],\
+            [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0],\
+            [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0],\
+            [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],\
+            [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0],\
+            [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0],\
+            [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0],\
+            [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0],\
+            [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0],\
+            [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0],\
+            [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0],\
+            [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0],\
+            [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0],\
+            [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0]]
+
 
 def criar_labirinto(p1=420, p2=420, p3=370, p4=0):
     """ Cria uma tela do Turtle """
@@ -33,33 +34,35 @@ def criar_labirinto(p1=420, p2=420, p3=370, p4=0):
     setup(p1, p2, p3, p4)
     # Para cada celula (i,j) da matriz que for caminho desenhe uma celula
     matriz = ler_matriz()
-    tam_matriz = len(matriz)
-    for lin in range(tam_matriz):
-        for col in range(tam_matriz):
+    lado = 20
+    tam_celula = 20
+    dimensao_da_matriz = len(matriz)
+    for lin in range(dimensao_da_matriz):
+        for col in range(dimensao_da_matriz):
             if (matriz[lin][col] == 1):
-                xt, yt = em_coord_turtle(lin,col)
-                desenhar_celula(xt, yt, 'blue')
+                x, y = em_coord_turtle(lin, col, dimensao_da_matriz, tam_celula)
+                desenhar_celula(x, y, 'blue', lado)
     update()
 
-def desenhar_celula(xt, yt, cor):
-    """ Dada uma coordenada (xt, yt) do Turtle, desenha um quadrado (célula) na posição """
+def desenhar_celula(x, y, cor, lado):
+    """ Dada uma coordenada (x, y) do Turtle, desenha um quadrado (célula) na posição """
     color(cor)
     up()
-    goto(xt,yt)
+    goto(x, y)
     down()
     begin_fill()
     for _ in range(4):
-        forward(20)
+        forward(lado)
         left(90)
     end_fill()
     up()
 
-def em_coord_turtle(xm, ym):
-    """ Dada uma coordenada da matriz (i,j) transforma em coordenada Turtle """
-    meio = 20 // 2
-    yt = (ym - meio) * 20
-    xt = (meio - xm) * 20
-    return yt, xt
+def em_coord_turtle(lin, col, dim, tam_celula):
+    """ Dada uma coordenada da matriz (lin,col) transforma em coordenada Turtle """
+    meio = dim // 2
+    x = (col - meio) * tam_celula
+    y = (meio - lin) * tam_celula
+    return x, y
 
 def main():
     criar_labirinto()
